0e5cd5a1a434a2e4ee4c9a4cfb67cc151daecf5d
Qianwei Zhou <zhouqianweischolar@gmail.com>
2024-01-21 16:51:57 +0800

    Update IXI_utils.py

--- IXI_utils.py
+++ IXI_utils.py
@@ -1,7 +1,3 @@
-"""
-Copyright (C) 2018 NVIDIA Corporation.  All rights reserved.
-Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode).
-"""
 from torch.utils.data import DataLoader
 from torch.autograd import Variable
 from torch.optim import lr_scheduler
